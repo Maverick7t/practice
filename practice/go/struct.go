05e8cf1fdbaca4b08154e943353409010e7d917c
Maverick7t <moinaktarshaikh651@gmail.com>
2026-07-27 12:00:00 +0000

    Add Go practice example 5: struct

---
+++ practice/go/struct.go
@@ -0,0 +1,14 @@
+﻿package main
+
+import "fmt"
+
+type Book struct {
+    Title string
+    Pages int
+}
+
+func main() {
+    book := Book{Title: "Go Basics", Pages: 200}
+    fmt.Printf("%s has %d pages\n", book.Title, book.Pages)
+}
+
