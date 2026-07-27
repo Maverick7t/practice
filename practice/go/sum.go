c69ac79a9c30790fa5f7bf39a59924d43afe0a5f
Maverick7t <moinaktarshaikh651@gmail.com>
2026-07-27 12:00:00 +0000

    Add Go practice example 3: sum

---
+++ practice/go/sum.go
@@ -0,0 +1,13 @@
+﻿package main
+
+import "fmt"
+
+func main() {
+    nums := []int{2, 4, 6, 8}
+    sum := 0
+    for _, n := range nums {
+        sum += n
+    }
+    fmt.Println("sum:", sum)
+}
+
