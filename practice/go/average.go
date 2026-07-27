f769222460016afadf3516114d1a9c2ca24ef77c
Maverick7t <moinaktarshaikh651@gmail.com>
2026-07-27 12:00:00 +0000

    Add Go practice example 8: average

---
+++ practice/go/average.go
@@ -0,0 +1,13 @@
+﻿package main
+
+import "fmt"
+
+func main() {
+    numbers := []float64{10, 20, 30, 40}
+    sum := 0.0
+    for _, n := range numbers {
+        sum += n
+    }
+    fmt.Println("average:", sum/float64(len(numbers)))
+}
+
