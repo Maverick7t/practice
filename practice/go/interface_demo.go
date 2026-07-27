5b440cf474bc36ff2acd301044299a13a4b40b49
Maverick7t <moinaktarshaikh651@gmail.com>
2026-07-27 12:00:00 +0000

    Add Go practice example 13: interface_demo

---
+++ practice/go/interface_demo.go
@@ -0,0 +1,17 @@
+﻿package main
+
+import "fmt"
+
+type Speaker interface {
+    Speak() string
+}
+
+type Dog struct{}
+
+func (d Dog) Speak() string { return "woof" }
+
+func main() {
+    var s Speaker = Dog{}
+    fmt.Println(s.Speak())
+}
+
