272d3051eb629859c4d8dfe4e17803fa5aadccaa
Maverick7t <moinaktarshaikh651@gmail.com>
2026-07-28 12:00:00 +0000

    Add Go practice example 13: day28_reverse

---
+++ practice/go/day28_reverse.go
@@ -0,0 +1,16 @@
+﻿package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    word := "go"
+    var reversed strings.Builder
+    for i := len(word) - 1; i >= 0; i-- {
+        reversed.WriteByte(word[i])
+    }
+    fmt.Println(reversed.String())
+}
+
