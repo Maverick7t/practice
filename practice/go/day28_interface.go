51887e4817dee2f1c28e92a30f8e2d6fe61be95d
Maverick7t <moinaktarshaikh651@gmail.com>
2026-07-28 12:00:00 +0000

    Add Go practice example 12: day28_interface

---
+++ practice/go/day28_interface.go
@@ -0,0 +1,17 @@
+﻿package main
+
+import "fmt"
+
+type Speaker interface {
+    Speak() string
+}
+
+type Cat struct{}
+
+func (c Cat) Speak() string { return "meow" }
+
+func main() {
+    var s Speaker = Cat{}
+    fmt.Println(s.Speak())
+}
+
